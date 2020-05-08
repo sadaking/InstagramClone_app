feb5d49592ef4abe174fef33e4e42951ee7cd825
sadaking <sichirigahama.37@gmail.com>
2020-05-08 17:07:02 +0900

    UserとPictureのアソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#new'
-  get '/blogs', to: 'blogs#index' 
+  get '/pictures', to: 'pictures#index' 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
