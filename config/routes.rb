1e3a3dcf840e5f78971693c92a0e88716e7497ad
sadaking <sichirigahama.37@gmail.com>
2020-05-08 15:42:59 +0900

    ログインセッション機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#new'
-  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
