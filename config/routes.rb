31b8166049ab42e8b70d7f5eb50fabcbf44d3b26
sadaking <sichirigahama.37@gmail.com>
2020-05-08 16:13:33 +0900

    投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#new'
+  get '/blogs', to: 'blogs#index' 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
