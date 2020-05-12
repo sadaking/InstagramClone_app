9c1bd114f247a4be7780119c1ab822ca4ce87af1
sadaking <sichirigahama.37@gmail.com>
2020-05-12 16:03:36 +0900

    フロントの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ before_action :set_user, only: [:show, :edit, :update, :favorites]
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path(@user.id)
     else
       render :new
     end
@@ -24,7 +24,7 @@ before_action :set_user, only: [:show, :edit, :update, :favorites]
     if current_user == @user
       if @user.update(user_params)
         flash[:success] = 'ユーザー情報を編集しました。'
-        render :edit
+        redirect_to user_path(@user.id)
       else
         flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
         render :edit
@@ -36,6 +36,14 @@ before_action :set_user, only: [:show, :edit, :update, :favorites]
 
   def favorites
     @favorites = Favorite.where(user_id: @user.id)
+  end
+
+  def ensure_correct_user
+    @user = User.find_by(id:params[:id])
+    if @user.user_id != current_user.id
+      flash[:notice] = "あなたには権限がありません！"
+      redirect_to users_path
+    end
   end
 
   private
